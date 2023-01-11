a3e105c5afd31d5837268cc84061167ca6dd3bd3
ClassicMC <classicmclive@gmail.com>
2023-01-11 23:38:54 +0000

    rs23

--- rs23/main.js
+++ rs23/main.js
@@ -6,6 +6,7 @@ let rocks = {r0:document.getElementById("r0"),
              r3:document.getElementById("r3")}
 let cr = -1;
 let amnt = 0;
+let opc = 1;
 function redraw(){
     bg("skyblue");
     noStroke();
@@ -16,19 +17,50 @@ function redraw(){
     else if(cr == 1){image(rocks.r1,window.innerWidth/2-(64*5/2),window.innerHeight/2-(64*5/2)-15,64*5,64*5);}
     else if(cr == 2){image(rocks.r2,window.innerWidth/2-(64*5/2),window.innerHeight/2-(64*5/2),64*5,64*5);}
     else if(cr == 3){image(rocks.r3,window.innerWidth/2-(64*5/2),window.innerHeight/2-(64*5/2)-15,64*5,64*5);}
-    if(cr == -1){
-        selectColor("black");
-        text("Rock Simulator",window.innerWidth/2-135,window.innerHeight/2-130,"36","sans-serif")
-        text("2023",window.innerWidth/2-45,window.innerHeight/2-100,"36","sans-serif")
+    else if(cr == 4){
+        bg("green");
+        image(rocks.r3,window.innerWidth/2-(64*5/2),window.innerHeight/2-(64*5/2)-15,64*5,64*5);
+        selectColor("darkgreen");
+        text(amnt.toString(),10,23,"24","sans-serif");
     }
+    else if(cr == 5){
+        bg("green");
+        image(rocks.r1,window.innerWidth/2-(64*5/2),window.innerHeight/2-(64*5/2)-15,64*5,64*5);
+        selectColor("darkgreen");
+        text(amnt.toString(),10,23,"24","sans-serif");
+    }
+    selectColor("black");
+    text("Rock Simulator",window.innerWidth/2-135,window.innerHeight/2-130,"36","sans-serif",opc)
+    text("2023",window.innerWidth/2-45,window.innerHeight/2-100,"36","sans-serif",opc)
+    selectColor("black");
+    selectColor("yellow");
+    ellipse(window.innerWidth-10,10,50);
 };redraw();
+function fade(){
+    if(opc >= 0.01){
+        opc -= 0.01
+        redraw();
+    }else{
+        opc = 0;
+    }
+}
 canvas.addEventListener('click', function(event) {
     next();
     amnt ++;
+    x = event.clientX;
+    y = event.clientY;
+    if(y <= 50){
+        if(cr == 0||cr == 1){
+            cr = 4;
+        }
+        else{
+            cr = 5
+        }
+    }
     redraw();
 });
 function next(){
-    if(cr == -1){cr = 0}
+    if(cr == -1){cr = 0;setInterval(fade,20)}
     if(cr == 0){cr = 1;}
     else if(cr == 1){cr = 2}
     else if(cr == 2){cr = 3}
